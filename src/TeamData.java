27cca32cba1c3b9b120a68a12644cc7c3b5a6c03
Mike Baldwin <mikebaldwin21@gmail.com>
2018-09-06 08:28:29 -0400

    Moved adding the player to the players list to after the switch statement, just to remove redundant lines.

--- src/TeamData.java
+++ src/TeamData.java
@@ -11,7 +11,7 @@ public class TeamData {
 	public static void main(String[] args) {
 		List<String> allStrings = new ArrayList<>();
 
-		try{
+		try {
 			allStrings = Files.readAllLines(Paths.get(System.getProperty("user.dir") + "/playerinfo.txt"));
 		} catch (FileNotFoundException exception) {
 			System.out.println("Failed to find playerinfo.txt, please make sure your file is spelled the correctly. " + exception);
@@ -19,45 +19,40 @@ public class TeamData {
 			System.out.println("Encountered an IO error: " + exception);
 		}
 
-		for(var i = 0; i < allStrings.size();) {
-			//Read the first line to determine what type of player=
-			switch(allStrings.get(i))
-			{
-				case "QuarterBack": {
-					Player player = new Quarterback();
+		for (var i = 0; i < allStrings.size(); ) {
+			//Read the first line to determine what type of player
+			Player player;
+			switch (allStrings.get(i)) {
+				case "QuarterBack":
+					player = new Quarterback();
 					player.LoadPlayerData(allStrings.subList(i, i + Player.RequiredInputCount + Quarterback.RequiredInputCount));
 					i += Player.RequiredInputCount + Quarterback.RequiredInputCount;
-					players.add(player);
-				}
-				break;
-				case "Defense": {
-					Player player = new Defense();
+					break;
+				case "Defense":
+					player = new Defense();
 					player.LoadPlayerData(allStrings.subList(i, i + Player.RequiredInputCount + Defense.RequiredInputCount));
 					i += Player.RequiredInputCount + Defense.RequiredInputCount;
-					players.add(player);
-				}
-				break;
-				case "Receiver": {
-					Player player = new Receiver();
+					break;
+				case "Receiver":
+					player = new Receiver();
 					player.LoadPlayerData(allStrings.subList(i, i + Player.RequiredInputCount + Receiver.RequiredInputCount));
 					i += Player.RequiredInputCount + Receiver.RequiredInputCount;
-					players.add(player);
-				}
-				break;
-					default: {
-						System.out.println("Encountered unknown player position [" + allStrings.get(i) + "] at line position [" + i + "]");
-						System.out.println("Please attempt to correct the error and try again.");
-						System.out.println("The following is a printout of all correctly loaded players so far:");
-						for (Player iPlayer : players) {
-							System.out.println(iPlayer);
-							System.out.println();
-						}
-					}
+					break;
+				default:
+					System.out.println("Encountered unknown player position [" + allStrings.get(i) + "] at line position [" + i + "]");
+					System.out.println("Please attempt to correct the error and try again.");
+					System.out.println("The following is a printout of all correctly loaded players so far:");
+					printAllPlayers();
 					return;
 			}
+			players.add(player);
 		}
 
 		System.out.println("The following is a printout of all correctly loaded players so far:");
+		printAllPlayers();
+	}
+
+	public static void printAllPlayers() {
 		for (Player iPlayer : players) {
 			System.out.println(iPlayer);
 			System.out.println();
