e22ce10ad4e44521cf884424838a0849e597d9be
Mike Baldwin <mikebaldwin21@gmail.com>
2018-09-06 22:11:29 -0400

    Added an InvalidPathException catch block, an inputFilePath field, and some comments.

--- src/Team.java
+++ src/Team.java
@@ -6,17 +6,25 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Team {
-	public static List<Player> players = new ArrayList<>();
+	private static final String inputFilePath = System.getProperty("user.dir") + "/playerinfo.txt";
+	private static List<Player> players = new ArrayList<>();
 
+	/*
+	 * A few things:
+	 * 1. We don't need to check if marking the stream is supported or not, BufferedReader does, that's the point of buffering it  :)
+	 * 2. I only mark 15 characters ahead because the largest line we will be reading is "Quarterback", and accounting for the carriage read at the end, that's only 12 characters.
+	 * 3. No "finally" block is needed as I'm using a Try-With-Resources block. NOTE: If it fails to compile for you, update to at least jre7.
+	 */
 	public static void main(String[] args) {
-		try (BufferedReader reader = Files.newBufferedReader(Paths.get(System.getProperty("user.dir") + "/playerinfo.txt"))) {
+		try (BufferedReader reader = Files.newBufferedReader(Paths.get(inputFilePath))) {
 			while (reader.ready()) {
-				reader.mark(15); //I think 15 characters is enough to account for the longest word "Quarterback", plus some
+				reader.mark(15);
 				String position = reader.readLine();
 				reader.reset();
 				Player player;
@@ -40,6 +48,8 @@ public class Team {
 				}
 				players.add(player);
 			}
+		} catch (InvalidPathException exception) {
+			System.out.println("The path provided to playerinfo.txt is invalid, please make sure you set the field \"inputFilePath\" to the correct path. " + exception);
 		} catch (FileNotFoundException exception) {
 			System.out.println("Failed to find playerinfo.txt, please make sure your file is spelled the correctly. " + exception);
 		} catch (IOException exception) {
