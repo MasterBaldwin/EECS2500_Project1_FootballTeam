a8f54aa05fb4abf6c77a3554beb3c287e6fa44ec
Mike Baldwin <mikebaldwin21@gmail.com>
2018-09-06 21:04:28 -0400

    Adders Getters and Setters to the Player class.

--- src/Player.java
+++ src/Player.java
@@ -6,19 +6,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public abstract class Player {
-	public String position;
-	public String name;
-	public float weight;
-	public int age;
+	private String position;
+	private String name;
+	private float weight;
+	private int age;
 
-	protected Player(String position) {
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
 		this.position = position;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	protected Player(String position) {
+		setPosition(position);
+	}
+
 	public void LoadPlayerData(BufferedReader reader) throws IOException {
-		name = reader.readLine();
-		weight = Float.parseFloat(reader.readLine());
-		age = Integer.parseInt(reader.readLine());
+		setName(reader.readLine());
+		setWeight(Float.parseFloat(reader.readLine()));
+		setAge(Integer.parseInt(reader.readLine()));
 		LoadInternalData(reader);
 	}
 
@@ -26,9 +58,9 @@ public abstract class Player {
 
 	@Override
 	public String toString() {
-		return "Position: " + position +
-				"\nName: " + name +
-				"\nWeight: " + weight +
-				"\nAge: " + age;
+		return "Position: " + getPosition() +
+				"\nName: " + getName() +
+				"\nWeight: " + getWeight() +
+				"\nAge: " + getAge();
 	}
 }
